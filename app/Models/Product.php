ad4836ffb4b15524e319e84b592dd4632e0d8497
anthoniasr <anthonia@bemobile.tech>
2025-10-17 19:46:28 -0300

    Initial setup: Laravel project, migrations, models and controllers

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'category_id',
+        'description',
+        'price',
+        'photo',
+        'expiration_date',
+        'sku'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function movements()
+    {
+        return $this->hasMany(Movement::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\MovementController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
+Route::resource('movements', MovementController::class);
 
 Route::get('/', function () {
     return view('welcome');
